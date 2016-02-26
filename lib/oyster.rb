58b35f2508fea4234e0cd559fc26bfe0005870f9
Brandon Graves <bgraves14@gmail.com>
2016-02-26 16:52:44 +0000

    added top up and deduct functionality

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,9 +1,20 @@
 class Oyster
 
+  BALANCE_LIMIT = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    fail 'Maximum balance of #{maximum_balance} exceeded' if @balance + amount > BALANCE_LIMIT
+    @balance += amount
+  end
+
+  def deduct_amount(amount)
+    @balance -= amount
   end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -6,4 +6,28 @@ describe Oyster do
     expect(subject.balance).to eq(0)
   end
 
+  it 'raises and error when maximum balance is exceeded' do
+    maximum_balance = Oyster::BALANCE_LIMIT
+    subject.top_up(maximum_balance)
+    expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+  end
+
+  describe '#deduct_amount' do
+    it { is_expected.to respond_to(:deduct_amount).with(1).argument }
+
+    it 'can deduct from balance' do
+      subject.top_up(5)
+      subject.deduct_amount(1)
+      expect(subject.balance).to eq(4)
+    end
+  end
+
 end
