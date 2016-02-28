81870fccbe3c5d8c5848b5954609709cd9070e69
Brandon Graves <bgraves14@gmail.com>
2016-02-28 19:29:45 +0000

    added journey history

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,12 +1,15 @@
 class Oyster
 
   BALANCE_LIMIT = 90
+  MINIMUM_BALANCE = 1
 
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :in_journey, :journey
+  attr_accessor :entry_station, :exit_station
 
   def initialize
     @balance = 0
     @in_journey = false
+    @journey = {}
   end
 
   def top_up(amount)
@@ -19,15 +22,27 @@ class Oyster
   end
 
   def in_journey?
-    @in_journey
+    !!entry_station
   end
 
-  def touch_in
+  def touch_in(entry_station)
+    @entry_station = entry_station
+    fail 'You must have a minimum of 1$ to touch in!' if @balance < MINIMUM_BALANCE
     @in_journey = true
+    @journey[:entry_station] = @entry_station
   end
 
-  def touch_out
-    @in_journey = false
+  def touch_out(exit_station)
+    deduct_amount(MINIMUM_BALANCE)
+    @exit_station = exit_station
+    @entry_station = nil
+    @journey[:exit_station] = @exit_station
   end
+
+  def journeys
+    @journey
+  end
+
+  private :deduct_amount
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -2,6 +2,9 @@ require 'oyster'
 
 describe Oyster do
 
+  let(:entry_station){double :station}
+  let(:exit_station){double :station}
+  let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
 
   it 'has a balance of 0' do
     expect(subject.balance).to eq(0)
@@ -11,18 +14,15 @@ describe Oyster do
     expect(subject).not_to be_in_journey
   end
 
-  it 'can touch in' do
-    subject.touch_in
-    expect(subject).to be_in_journey
+  it 'has an empty journey log' do
+    expect(subject.journeys).to be_empty
   end
 
-  it 'can touch out' do
-    subject.touch_in
-    subject.touch_out
-    expect(subject).not_to be_in_journey
+
+  it 'will only let people touch in if balance is minimum of $1' do
+    expect{ subject.touch_in(entry_station) }.to raise_error 'You must have a minimum of 1$ to touch in!'
   end
 
-  it { is_expected.to respond_to(:deduct_amount).with(1).argument }
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
@@ -33,22 +33,42 @@ describe Oyster do
     end
   end
 
-  describe 'added balance needed' do
+  describe 'in journey' do
 
     before(:each) do
       subject.top_up(Oyster::BALANCE_LIMIT)
+      subject.touch_in(entry_station)
+    end
+
+    it 'can touch in' do
+      expect(subject).to be_in_journey
+    end
+
+    it 'can touch out' do
+      subject.touch_out(exit_station)
+      expect(subject).not_to be_in_journey
+    end
+
+    it 'will charge when you touch out' do
+      expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by(-Oyster::MINIMUM_BALANCE)
+    end
+
+    it 'stores a entry station' do
+      expect(subject.entry_station).to eq entry_station
+    end
+
+    it 'stores a exit station' do
+      subject.touch_out(exit_station)
+      expect(subject.exit_station).to eq exit_station
+    end
+
+    it 'stores a journey' do
+      subject.touch_out(exit_station)
+      expect(subject.journeys).to include journey
     end
 
     it 'raises and error when maximum balance is exceeded' do
       expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
-    end
-
-    describe '#deduct_amount' do
-
-      it 'can deduct from balance' do
-        subject.deduct_amount(1)
-        expect(subject.balance).to eq(Oyster::BALANCE_LIMIT - 1)
-      end
     end
   end
 
