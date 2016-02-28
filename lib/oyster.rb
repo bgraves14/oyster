2b7ff6b6fc11e40fffc8bdf473cadf6de59c3c0e
Brandon Graves <bgraves14@gmail.com>
2016-02-28 16:48:38 +0000

    added touch in/out functionality

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -2,10 +2,11 @@ class Oyster
 
   BALANCE_LIMIT = 90
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -15,6 +16,18 @@ class Oyster
 
   def deduct_amount(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -2,31 +2,53 @@ require 'oyster'
 
 describe Oyster do
 
+
   it 'has a balance of 0' do
     expect(subject.balance).to eq(0)
   end
 
-  it 'raises and error when maximum balance is exceeded' do
-    maximum_balance = Oyster::BALANCE_LIMIT
-    subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+  it 'is initially not in a journey' do
+    expect(subject).not_to be_in_journey
   end
 
+  it 'can touch in' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it 'can touch out' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+
+  it { is_expected.to respond_to(:deduct_amount).with(1).argument }
+
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up the balance' do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
   end
 
-  describe '#deduct_amount' do
-    it { is_expected.to respond_to(:deduct_amount).with(1).argument }
+  describe 'added balance needed' do
 
-    it 'can deduct from balance' do
-      subject.top_up(5)
-      subject.deduct_amount(1)
-      expect(subject.balance).to eq(4)
+    before(:each) do
+      subject.top_up(Oyster::BALANCE_LIMIT)
+    end
+
+    it 'raises and error when maximum balance is exceeded' do
+      expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+    end
+
+    describe '#deduct_amount' do
+
+      it 'can deduct from balance' do
+        subject.deduct_amount(1)
+        expect(subject.balance).to eq(Oyster::BALANCE_LIMIT - 1)
+      end
     end
   end
 
